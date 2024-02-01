94cd37f5347a990e7b554035490624f9f125ff5e
mateusfl1998 <154616064+mateusfl1998@users.noreply.github.com>
2024-02-01 10:11:26 -0300

    alterei o hhtp response

--- Projeto Carros/cars/views.py
+++ Projeto Carros/cars/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+def cars_view(request):
+    return HttpResponse('''<h1>Meus Carros</h1>
+                        Só carro top na Mtec Carros!    
+                        ''')
