7f2b71b8a997ebaf6a884a0d6760a0ee0db560c0
mateusfl1998 <154616064+mateusfl1998@users.noreply.github.com>
2024-02-07 19:48:12 -0300

    dia-07-02-2024

--- Projeto Carros/cars/models.py
+++ Projeto Carros/cars/models.py
@@ -21,3 +21,4 @@ class Car(models.Model):
 
     def __str__(self):
         return self.model
+    
--- Projeto Carros/cars/views.py
+++ Projeto Carros/cars/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from cars.models import Car
+from cars.forms import CarForm
 
 def cars_view(request):
-    return HttpResponse('''<h1>Meus Carros</h1>
-                        Só carro top na Mtec Carros!    
-                        ''')
+    cars = Car.objects.all()
+    return render (    
+    request,
+    'cars.html',
+    {'cars': cars}
+    )
+
+def new_car_view(request):
+    new_car_form = CarForm()
+    if request.method == 'POST':
+        new_car_form = CarForm(request.POST, request.FILES)
+        print(new_car_form.data)
+    else:
+        return render(request, 'new_car.html', {'new_car_form':new_car_form})
