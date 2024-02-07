e55bea80ececb14984f6d9ca12b964fbe5fa93bc
mateusfl1998 <154616064+mateusfl1998@users.noreply.github.com>
2024-02-07 20:04:06 -0300

    alteracao - 2

--- Projeto Carros/cars/forms.py
+++ Projeto Carros/cars/forms.py
@@ -1,6 +1,6 @@
 from django import forms
-from cars.models import Brand
-""
+from cars.models import Brand,Car
+
 class CarForm(forms.Form):
     model = forms.CharField(max_length=200)
     brand = forms.ModelChoiceField(Brand.objects.all())
@@ -9,3 +9,17 @@ class CarForm(forms.Form):
     plate = forms.CharField(max_length=10)
     value = forms.FloatField()
     photo = forms.ImageField()
+
+    def save(self):
+        car = Car(
+            model = self.cleaned_data['model'],
+            brand = self.changed_data['brand'],
+            factory_year = self.changed_data['factory_year'],
+            model_year = self.changed_data['model_year'],
+            plate = self.changed_data['plate'],
+            value = self.changed_data['value'],
+            photo = self.changed_data['photo'],
+
+        )
+        car.save()
+        return car
--- Projeto Carros/cars/views.py
+++ Projeto Carros/cars/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from cars.models import Car
 from cars.forms import CarForm
@@ -12,9 +12,12 @@ def cars_view(request):
     )
 
 def new_car_view(request):
-    new_car_form = CarForm()
     if request.method == 'POST':
         new_car_form = CarForm(request.POST, request.FILES)
+        if new_car_form.is_valid():
+            new_car_form.save()
+            return redirect('car_list')
         print(new_car_form.data)
     else:
+        new_car_form = CarForm()
         return render(request, 'new_car.html', {'new_car_form':new_car_form})
