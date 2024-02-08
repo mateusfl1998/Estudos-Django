8a85f35efe0dd7175483797377fbec13b3decb1f
mateusfl1998 <154616064+mateusfl1998@users.noreply.github.com>
2024-02-08 19:28:07 -0300

    trquei form pelo model.forms do django

--- Projeto Carros/cars/forms.py
+++ Projeto Carros/cars/forms.py
@@ -13,13 +13,18 @@ class CarForm(forms.Form):
     def save(self):
         car = Car(
             model = self.cleaned_data['model'],
-            brand = self.changed_data['brand'],
-            factory_year = self.changed_data['factory_year'],
-            model_year = self.changed_data['model_year'],
-            plate = self.changed_data['plate'],
-            value = self.changed_data['value'],
-            photo = self.changed_data['photo'],
+            brand = self.cleaned_data['brand'],
+            factory_year = self.cleaned_data['factory_year'],
+            model_year = self.cleaned_data['model_year'],
+            plate = self.cleaned_data['plate'],
+            value = self.cleaned_data['value'],
+            photo = self.cleaned_data['photo'],
 
         )
         car.save()
         return car
+
+class CarModelForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields ='__all__'
